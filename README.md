fcfbdc3dcd2310a4b81732b382007c14a12b5a1c
Dhaval Metrani <Dhaval@android-89bdc7563562549f.ads.autodesk.com>
2016-04-06 14:21:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# spacewalk
+User story: 
+
+I. Write a tool which would: 
+1. For every AWS root account, have baseline for number of EC2 instances, RDS instances, EBS volumes, etc.
+2. Get the number of instances for EC2, RDS, EBS, etc.
+3. Based on the price of EC2, RDS, S3, etc, get the total cost of existing resources.
+4. Get the number of redundant instances for these services.
+5. Based on this, get the cost of redundant instances per month.
---
+++ get_ebs_volumes.py
@@ -0,0 +1,70 @@
+
+################################################
+__author__ = "Dhaval Mertani"
+__copyright__ = "Copyright 2016, Autodesk"
+__credits__ = ["Dhaval Metrani"]
+__license__ = "Autodesk License"
+__version__ = "1.0.0"
+__maintainer__ = "Dhaval Metrani"
+__email__ = "dhaval.metrani@autodesk.com"
+__status__ = "Prototype"
+################################################
+
+
+
+################################################
+import argparse, sys
+import datetime, os
+################################################
+import boto3
+# import botocore.session
+################################################
+from lib.constants import *
+from lib.aws_util import *
+from lib.util import *
+################################################
+
+
+################################################
+def main(argv):
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-ap", "--aws-profile", help="Provide name of AWS profile")	
+	# parser.add_argument("-o", "--output-file", help="Provide path to output file")	
+	# parser.add_argument("-ac", "--aws-config", help="Provide name of json file containing AWS configs")
+	args = parser.parse_args()
+	aws_profile = args.aws_profile
+
+	if not aws_profile:
+		aws_profile = Constants.AWS_PROFILE_DEFAULT
+	# if not output_file:
+	# 	output_file = Constants.OUTPUT_FILE
+
+	output_file = aws_profile + Constants.OUTPUT_FORMAT
+
+
+	print "[INFO] Using AWS profile: " + aws_profile
+
+	use_profile(aws_profile)
+
+	now = datetime.datetime.now()
+	output_folder = "./" + Constants.OUTPUT_FOLDER + "/" + "%s" % now
+	create_folder(output_folder)
+	output_file = os.path.join(output_folder, output_file)
+	
+	
+	# prepare_aws_ebs_volume_report(output_file, volumes=Constants.AWS_VOLUMES_ALL)
+	# prepare_aws_ebs_volume_report(output_file, volumes=Constants.AWS_VOLUMES_INUSE)
+	prepare_aws_ebs_volume_report(output_file, volumes=Constants.AWS_VOLUMES_AVAILABLE)
+
+	print "[INFO] Done. Output: " + output_file
+	# print volume_details
+
+	send_email()
+
+
+
+################################################
+if __name__ == "__main__":
+	main(sys.argv[1:])
+################################################
